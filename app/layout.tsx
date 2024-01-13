408bdd50f1bd623adedeef512f7e099eda47a7bb
Katerin <katerinmonguacastro@gmail.com>
2024-01-12 20:03:24 -0500

    Added more elements to the Intro component with Tailwind css.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,13 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-purple-100 text-gray-950 relative h-[5000px] pt-36 sm:pt-36`}>
-        <div className="bg-[#e78686] absolute top-[-6rem] -z-10 rigth-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        className={`${inter.className} bg-purple-100 text-gray-950 relative pt-28 sm:pt-28`}>
+        {/* DIVS ON THE RIGHT CORNER */}
+        <div className="bg-[#e78686] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#f6bbff] absolute top-[-1rem] -z-10 left-[-36rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
         <Header />
+
         {children}
       </body>
     </html>
--- components/intro.tsx
+++ components/intro.tsx
@@ -5,6 +5,11 @@ import React from "react";
 import { motion, spring } from "framer-motion";
 import imgProfile from "@/public/imgprofile2.jpg";
 import { Roboto_Mono } from "next/font/google";
+import Link from "next/link";
+//! REACT ICONS
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
 
 export const roboto_mono = Roboto_Mono({
   subsets: ["latin"],
@@ -114,7 +119,7 @@ export default function Intro() {
         </div>
       </div>
       <motion.p
-        className={`${roboto_mono.className} font-normal mb-10 mt-10 px-4xl text-2xl !leading-[1.1] sm:text-4xl lg:!leading-[1.2]`}
+        className={`${roboto_mono.className} font-normal mb-10 mt-8 px-4 text-2xl !leading-[1.1] sm:text-4xl lg:!leading-[1.2]`}
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}>
@@ -128,6 +133,35 @@ export default function Intro() {
         {`I've also worked with the `}
         <span className="font-bold">MERN and PERN</span> stacks.
       </motion.p>
+
+      <div
+        className={`${roboto_mono.className} flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium`}>
+        <Link
+          href={"#contact"}
+          className="group bg-purple-700 text-white px-7 py-3 rounded-full flex items-center gap-2 outline-none focus:scale-110 hover:scale-105 hover:bg-gray-950 active:scale-105 transition">
+          Contact me
+          <BsArrowRight className="opacity-80 group-hover:translate-x-1 rounded-full h-6 w-6 transition hover:text-purple-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50" />
+        </Link>
+        <a
+          href="/CV.pdf"
+          download
+          className="bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 cursor-pointer">
+          Download CV
+          <HiDownload className="opacity-80 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          href="https://github.com/Katemoca"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.3rem] rounded-full focus:scale-[1.15 hover:text-gray-950 active:scale-105 transition cursor-pointer ">
+          <FaGithubSquare />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/katerincastromongua/"
+          target="_blank"
+          className="bg-white p-4 text-gray-700  hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] active:scale-105 transition cursor-pointer">
+          <BsLinkedin />
+        </a>
+      </div>
     </section>
   );
 }
