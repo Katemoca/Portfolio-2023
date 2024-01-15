2c819337a38e835f1cdd0008cf758b8210c07c20
Katerin <katerinmonguacastro@gmail.com>
2024-01-15 10:01:37 -0500

    Modified section divider

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -1,26 +1,19 @@
-"use client";
-
-import React from "react";
-import { motion } from "framer-motion";
-
-export default function SectionDivider() {
+export const SectionDivider = ({ height = 1 }) => {
   return (
-    <motion.div
-      className="my-24 flex justify-center"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.125 }}>
+    <div className="relative w-full -z-10 mt-64">
       <svg
-        width="100"
-        height="100" // Increase height to accommodate the flower
-        viewBox="0 0 100 80" // Adjust viewBox accordingly
-        xmlns="http://www.w3.org/2000/svg">
-        {/* First Wave */}
-        <path d="M0 35l10-10c5-5 15-5 20 0l10 10" fill="#007BFF" />
-        <path d="M50 0v50" stroke="#007BFF" strokeWidth="3" />
-        {/* Second Wave */}
-        <path d="M100 35l-10-10c-5-5-15-5-20 0l-10 10" fill="#007BFF" />
+        className={`absolute w-full h-${height} top-1/2 transform -translate-y-1/2`}
+        viewBox="0 0 100 100"
+        preserveAspectRatio="none">
+        <path
+          d="M0 50 C15 25,25 75,50 50 C75 25,85 75,100 50"
+          fill="none"
+          stroke="rgba(255, 255, 255, 0.5)"
+          strokeWidth="2"
+        />
       </svg>
-    </motion.div>
+    </div>
   );
-}
+};
+
+export default SectionDivider;
