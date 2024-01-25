450e3fac7da0c5ea862adc2da5ff1dfbe94c7250
Katerin <katerinmonguacastro@gmail.com>
2024-01-25 11:47:51 -0500

    Fixed bug on the experience section regarding the VerticalTimelineElement.

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import type { SectionName } from "./types";
 
-export function useSectionInView(sectionName: SectionName, threshold = 0.75){
+export function useSectionInView(sectionName: SectionName, threshold = 0.2){
     const { ref, inView } = useInView({
         threshold,
     })
@@ -21,6 +21,6 @@ export function useSectionInView(sectionName: SectionName, threshold = 0.75){
 
 
     return {
-        ref
+        ref, inView
     }
 }
